d6f32e1e15c1b2c571d6c318cbef7d5559da37e9
W Van leunen <willem@sjbmedia.nl>
2016-11-03 22:18:49 +0100

    vibrate phone deleted (not working with webGL) Panda TAG now goes to scene with Panda and the Sycamore Tree

---
+++ Assets/Vuforia/Scripts/pandaTrackerBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace Vuforia
+{
+	/// <summary>
+	/// A custom handler that implements the ITrackableEventHandler interface.
+	/// </summary>
+	public class pandaTrackerBehaviour : MonoBehaviour,
+	ITrackableEventHandler
+	{
+		#region PRIVATE_MEMBER_VARIABLES
+
+		private TrackableBehaviour mTrackableBehaviour;
+
+		#endregion // PRIVATE_MEMBER_VARIABLES
+
+
+
+		#region UNTIY_MONOBEHAVIOUR_METHODS
+
+		void Start()
+		{
+			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+			if (mTrackableBehaviour)
+			{
+				mTrackableBehaviour.RegisterTrackableEventHandler(this);
+			}
+		}
+
+		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+		#region PUBLIC_METHODS
+
+		/// <summary>
+		/// Implementation of the ITrackableEventHandler function called when the
+		/// tracking state changes.
+		/// </summary>
+		public void OnTrackableStateChanged(
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+			{
+				OnTrackingFound();
+			}
+			else
+			{
+				OnTrackingLost();
+			}
+		}
+
+		#endregion // PUBLIC_METHODS
+
+
+
+		#region PRIVATE_METHODS
+
+
+		private void OnTrackingFound()
+		{
+			Application.LoadLevel ("Demo_Scene"); 
+
+			Debug.Log("PANDA is on the move, I repeat...");
+		}
+
+
+		private void OnTrackingLost()
+		{
+			
+			Debug.Log("We lost the PANDA");
+		}
+
+		#endregion // PRIVATE_METHODS
+	}
+}
